9702d21dc7e6bf35df0c5e93e037ba5e9ba3d202
sakukaffe <binichgta@gmail.com>
2022-07-03 11:28:49 +0200

    AC

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,12 @@
 fx_version 'adamant'
 
 author 'Gamings System | Gaming_heart#1754'
-description 'GS-AC: AntiCheat by Gaming_heart#1754 for RinoV'
+description 'GS-AC: AntiCheat by Gaming_heart#1754 für RinoV'
 version 'v1.1'
 
 game 'gta5'
 
 client_scripts {
-    '@menuv/menuv.lua',
     'configs/client_config.lua',
     'client/*.lua'
 }
